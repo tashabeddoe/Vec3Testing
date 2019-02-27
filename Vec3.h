9eb32b02459d88cb9aa13c063377ecb93d4fe4d8
tashabeddoe <tasha.beddoe@gmail.com>
2019-02-27 13:24:40 +0000

    added defauly ctor

--- Vec3.h
+++ Vec3.h
@@ -5,11 +5,19 @@ class Vec3
 {
   public :
     Vec3()=default;
+
+    Vec3(float _x, float _y, float _z) :
+        m_x{_x}, m_y{_y}, m_z{_z}
+    {}
+
     ~Vec3() noexcept =default;
     Vec3(const Vec3 &)=default;
     Vec3 & operator=(const Vec3 &)=default;
     Vec3(Vec3 &&)=default;
     Vec3 & operator=(Vec3 &&)=default;
+    float m_x=0.0f;
+    float m_y=0.0f;
+    float m_z=0.0f;
 };
 
 #endif
--- Vec3Tests.cpp
+++ Vec3Tests.cpp
@@ -4,7 +4,20 @@
 using namespace ::testing;
 // for more details on gtest see here https://github.com/google/googletest/blob/master/googletest/docs/primer.md
 // my lecture https://nccastaff.bournemouth.ac.uk/jmacey/PP/slides/testing/slides.html#/
-TEST(Vec3, fail)
+TEST(Vec3, testDefaultConstrustor)
 {
-    ASSERT_EQ(1,1);
+    Vec3 a;
+    ASSERT_FLOAT_EQ(a.m_x,0.0f);
+    ASSERT_FLOAT_EQ(a.m_y,0.0f);
+    ASSERT_FLOAT_EQ(a.m_z,0.0f);
+}
+
+TEST(Vec3, userDefinedConstructor)
+{
+    Vec3 a(1.0f,2.0f,3.0f);
+    ASSERT_FLOAT_EQ(a.m_x,1.0f);
+    ASSERT_FLOAT_EQ(a.m_y,2.0f);
+    ASSERT_FLOAT_EQ(a.m_z,3.0f);
+
+
 }
